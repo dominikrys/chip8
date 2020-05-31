cc5c2d3eefa436653b703e1ac456264db9c6d671
dominikrys <dominik.j.rys@gmail.com>
2020-05-31 19:53:15 +0100

    Add Emscripten main method

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,7 +5,13 @@
 #include "Renderer.h"
 #include "Timer.h"
 
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#endif
+
 #include <iostream>
+
+#ifndef __EMSCRIPTEN__
 
 int main(int argc, char **argv) {
     try
@@ -61,3 +67,60 @@ int main(int argc, char **argv) {
 
     return 0;
 }
+
+#else
+
+Config config{};
+Chip8 chip8{config.mode_};
+KeyboardHandler keyboardHandler(chip8.keys());
+Renderer renderer{"CHIP-8 Emulator", VIDEO_WIDTH, VIDEO_HEIGHT, config.videoScale_};
+Audio audio{config.mute_};
+
+// 10 cycles per tick seems to more or less correspond to 750Hz on 60FPS display
+const int cyclesPerTick = 10;
+
+void loopFunction() {
+    if (keyboardHandler.handle())
+    {
+        emscripten_cancel_main_loop();
+    }
+
+    for (int i = 0; i < cyclesPerTick; i++)
+    {
+        chip8.cycle();
+    }
+
+    if (chip8.drawFlag())
+    {
+        auto buffer = chip8.video();
+        renderer.update(buffer, sizeof(buffer[0]) * VIDEO_WIDTH);
+
+        chip8.disableDrawFlag();
+    }
+    else if (chip8.soundFlag())
+    {
+        //audio.play(); TODO: add audio back in
+
+        chip8.disableSoundFlag();
+    }
+}
+
+int main() {
+    try
+    {
+        config.romPath_ = "../bin/roms/revival/games/Pong [Paul Vervalin, 1990].ch8";
+
+        chip8.loadRom(config.romPath_);
+
+        emscripten_set_main_loop(loopFunction, 0, 1);
+    }
+    catch (const std::exception &e)
+    {
+        std::cerr << e.what();
+        std::exit(EXIT_FAILURE);
+    }
+
+    return 0;
+}
+
+#endif
